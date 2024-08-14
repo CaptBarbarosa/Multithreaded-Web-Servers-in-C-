c634a613a07a4e5d73b9b98101a89fd1b8ba5c6c
CaptBarbarosa <tandogankaan5@gmail.com>
2024-08-14 17:37:37 +0100

    TCP_New_Connection_Acceptor is added

--- Project/TCP_New_Connection_Acceptor.h
+++ Project/TCP_New_Connection_Acceptor.h
@@ -1,1 +1,17 @@
-#ifndef
+#ifndef TCP_New_Connection_Acceptor
+#define TCP_New_Connection_Acceptor.h
+
+class TCP_Server_Controller;
+
+
+class TCP_New_Connection_Acceptor{
+    private:
+
+    public:
+        TCP_Server_Controller *tcp_ctrl;
+        TCP_New_Connection_Acceptor();
+        ~TCP_New_Connection_Acceptor(TCP_Server_Controller *);
+
+};
+
+#endif
