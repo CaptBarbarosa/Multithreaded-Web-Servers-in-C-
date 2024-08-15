4eb6f0b2c603464c784b6658e231bcc2facd072e
CaptBarbarosa <tandogankaan5@gmail.com>
2024-08-15 07:56:56 +0100

    Homework is done

--- Project/homework/main.cpp
+++ Project/homework/main.cpp
@@ -6,6 +6,8 @@ int main(){
     uint32_t ip_addr= 3232235777;
     char *returned = network_convert_ip_n_to_p(ip_addr, output_buffer);
     std:: cout << "Returned is: "<<returned << "\n\n";
+    uint32_t char_to_uint = network_convert_ip_p_to_n(returned);
+    std::cout << "Char to unit is: "<< char_to_uint << "\n\n";
     return 0;
 }
 
--- Project/homework/network_utils.cpp
+++ Project/homework/network_utils.cpp
@@ -4,40 +4,30 @@
 #include <cstdlib>  // for atoi
 
 char *network_convert_ip_n_to_p(uint32_t ip_addr, char *output_buffer) {
-    // Convert the IP address to A.B.C.D format
-    static char static_buffer[16]; // static buffer for the case where output_buffer is NULL
-    
+    static char static_buffer[16]; 
     if (output_buffer == NULL) {
         output_buffer = static_buffer;
     }
-
-    sprintf(output_buffer, "%u.%u.%u.%u",
-            (ip_addr >> 24) & 0xFF,  // First octet
-            (ip_addr >> 16) & 0xFF,  // Second octet
-            (ip_addr >> 8) & 0xFF,   // Third octet
-            ip_addr & 0xFF);         // Fourth octet
-
+    sprintf(output_buffer, "%u.%u.%u.%u", 
+            (ip_addr >> 24) & 0xFF, 
+            (ip_addr >> 16) & 0xFF,
+            (ip_addr >> 8) & 0xFF,
+            ip_addr & 0xFF);
     return output_buffer;
 }
-
 uint32_t network_convert_ip_p_to_n(const char *ip_addr) {
     uint32_t result = 0;
     int octet;
     char *token;
     char ip_copy[16];
-
-    // Copy the IP address to a local buffer since strtok modifies the string
     strncpy(ip_copy, ip_addr, sizeof(ip_copy));
     ip_copy[sizeof(ip_copy) - 1] = '\0';
-
-    // Split the IP address by '.' and convert each part to an integer
     token = strtok(ip_copy, ".");
     for (int i = 0; i < 4 && token != NULL; ++i) {
         octet = atoi(token);
         result |= (octet << (24 - 8 * i));
         token = strtok(NULL, ".");
     }
-
     return result;
 }
 
